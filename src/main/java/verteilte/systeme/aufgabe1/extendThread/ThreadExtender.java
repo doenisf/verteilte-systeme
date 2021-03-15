c2cb7fa5aac49d7bb9fa2e47adfcbc13a0fccb17
Simon Fröhner <simon@wk-froehner.de>
2021-03-15 22:21:36 +0100

    Aufgabe 1 fixed

--- src/main/java/verteilte/systeme/aufgabe1/extendThread/ThreadExtender.java
+++ src/main/java/verteilte/systeme/aufgabe1/extendThread/ThreadExtender.java
@@ -1,11 +1,16 @@
 package verteilte.systeme.aufgabe1.extendThread;
 
 public class ThreadExtender extends Thread{
+    long timeout;
+
+    ThreadExtender(long timeout) {
+        this.timeout = timeout;
+    }
 
     public static void main(String[] args) {
-        ThreadExtender t1 = new ThreadExtender();
+        ThreadExtender t1 = new ThreadExtender(500);
         t1.setName("hip");
-        ThreadExtender t2 = new ThreadExtender();
+        ThreadExtender t2 = new ThreadExtender(1000);
         t2.setName("hop");
         t1.start();
         t2.start();
@@ -13,22 +18,16 @@ public class ThreadExtender extends Thread{
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
+        while (true) {
             writeName(getName());
         }
     }
 
     public synchronized void writeName(String name) {
         try {
-            wait(100);
+            wait(timeout);
         } catch (InterruptedException e) {};
         System.out.println(name);
         notify();
     }
-
-    /*
-    "in einer Endlosschleife unterschiedlich schnell ausgibt"
-    Da kannst du auch schauen ob es einen anderen Weg gibt dies zu realisieren. Im Moment beruht deine Ausgabe auf
-    dem Scheduler. Unterschiedlich schnelle Ausgaben sind nicht implementiert.
-     */
 }
