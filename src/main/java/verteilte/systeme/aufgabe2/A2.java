5267b38c5c8145d4cf95c9e8a6f0f94c7f0adad7
Simon Fröhner <simon@wk-froehner.de>
2021-03-15 14:31:24 +0100

    Aufgabe 2 done

---
+++ src/main/java/verteilte/systeme/aufgabe2/A2.java
@@ -0,0 +1,9 @@
+package verteilte.systeme.aufgabe2;
+
+public class A2 {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + " - Prio: " + Thread.currentThread().getPriority());
+    }
+
+}
