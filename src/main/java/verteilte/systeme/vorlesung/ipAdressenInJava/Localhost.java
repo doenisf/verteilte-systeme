c37064533b8fabc7dc03df3791b1a87e4a41cfc6
Simon Fröhner <simon@wk-froehner.de>
2021-03-16 09:39:05 +0100

    Added example class from lecture

---
+++ src/main/java/verteilte/systeme/vorlesung/ipAdressenInJava/Localhost.java
@@ -0,0 +1,16 @@
+package verteilte.systeme.vorlesung.ipAdressenInJava;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Localhost {
+
+    public static void main(String[] args) throws UnknownHostException {
+
+        InetAddress address = InetAddress.getLocalHost();
+
+        System.out.printf("%s/%s%n", address.getHostName(), address.getHostAddress());
+
+    }
+
+}
